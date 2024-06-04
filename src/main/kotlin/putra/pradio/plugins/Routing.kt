7f759cf1a16ccdd3de7083ca2084c042783e9672
pradio-ep <pradio.ep@gmail.com>
2024-06-04 14:59:36 +0700

    Implement root path

--- src/main/kotlin/putra/pradio/plugins/Routing.kt
+++ src/main/kotlin/putra/pradio/plugins/Routing.kt
@@ -1,11 +1,15 @@
 package putra.pradio.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import putra.pradio.routes.pokemonRoutes
 
 fun Application.configureRouting() {
     routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
         pokemonRoutes()
     }
 }
